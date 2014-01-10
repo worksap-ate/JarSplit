e735a08b389dc2bfbd32fb3e3d3769bd4eda67e3
cloverrose <a_osari@worksap.co.jp>
2014-01-10 16:44:29 +0900

    bugfix(in MyClassVisitor select add or addSuper2Subs) and refactor(do not use isA hasA)

--- src/split/Spliter.java
+++ src/split/Spliter.java
@@ -19,39 +19,9 @@ public class Spliter {
 	}
 
 	public List<Set<String>> split(Map<String, Set<String>> dependency, Map<String, Set<String>> super2subs, int numRoots){
-		// make isA
-		Map<String, Set<String>> isA = new THashMap<String, Set<String>>();
-		for(Map.Entry<String, Set<String>> e : super2subs.entrySet()){
-			String superName = e.getKey();
-			for(String childName : e.getValue()){
-				if(!isA.containsKey(childName)){
-					isA.put(childName, new THashSet<String>());
-				}
-				isA.get(childName).add(superName);
-			}
-		}
-
-		// make hasA
-		Map<String, Set<String>> hasA = new THashMap<String, Set<String>>();
-		for(Map.Entry<String, Set<String>> e : dependency.entrySet()){
-			String ownerName = e.getKey();
-			for(String instanceName : e.getValue()){
-				if(!isA.containsKey(ownerName) || !isA.get(ownerName).contains(instanceName)){
-					if(!hasA.containsKey(ownerName)){
-						hasA.put(ownerName, new THashSet<String>());
-					}
-					hasA.get(ownerName).add(instanceName);
-				}
-			}
-		}
-		
-//		Set<String> roots = new HashSet<String>();
-//		roots.addAll(hasA.keySet());
-//		roots.addAll(isA.keySet());
-//
 		// addEdge owner -> instance
 		Graph<String> graph = new Graph<String>();
-		for(Map.Entry<String, Set<String>> e : hasA.entrySet()){
+		for(Map.Entry<String, Set<String>> e : dependency.entrySet()){
 			String ownerName = e.getKey();
 			for(String instanceName : e.getValue()){
 				graph.addEdge(ownerName, instanceName);
@@ -60,26 +30,22 @@ public class Spliter {
 		}
 
 		// addEdge child -> parent
-		for(Map.Entry<String, Set<String>> e : isA.entrySet()){
-			String childName = e.getKey();
-			for(String parentName : e.getValue()){
+		for(Map.Entry<String, Set<String>> e : super2subs.entrySet()){
+			String parentName = e.getKey();
+			for(String childName : e.getValue()){
 				graph.addEdge(childName, parentName);
-				// roots.remove(parentName);
 			}
 		}
 
 		// addEdge owner -> instance -> child ( -> child)*
-		for(Map.Entry<String, Set<String>> e : hasA.entrySet()){
+		for(Map.Entry<String, Set<String>> e : dependency.entrySet()){
 			String ownerName = e.getKey();
 			Set<String> work = new THashSet<String>();
 			for(String instanceName : e.getValue()){
 				addSpecialEdge(ownerName, instanceName, super2subs, work);
 			}
 			for(String n : work){
-				if(!n.equals(ownerName)){
-					graph.addEdge(ownerName, n);
-					// roots.remove(n);
-				}
+				graph.addEdge(ownerName, n);
 			}
 		}
 
