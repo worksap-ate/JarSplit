390d221bdf1541b0dd9a15612b5a8f9109375863
cloverrose <a_osari@worksap.co.jp>
2014-03-12 15:58:21 +0900

    remove unused functions and update partition method

--- src/main/java/com/github/cloverrose/jarsplit/partition/Partition.java
+++ src/main/java/com/github/cloverrose/jarsplit/partition/Partition.java
@@ -10,173 +10,15 @@ public class Partition {
 			this.y = y;
 		}
 	}
-	
-//	private double calcCost(List<List<Integer>> S){
-//		if(S.isEmpty()){
-//			return 0;
-//		}
-//		int l = S.get(0).size();
-//		int ret = 0;
-//		for(int i=0;i<l;i++){
-//			for(List<Integer> point : S){
-//				if(point.get(i) > 0){
-//					ret++;
-//					break;
-//				}
-//			}
-//		}
-//		return Math.pow(ret, 2);
-//	}
-	
-//	private double calcF(List<List<List<Integer>>> clusters){
-//		double ret = 0.0;
-//		for(List<List<Integer>> S : clusters){
-//			ret += calcCost(S);
-//		}
-//		return ret;
-//	}
 
-//	private List<List<List<Integer>>> assignsToClusters(List<List<Integer>> points, List<Integer> assigns, int k){
-//		List<List<List<Integer>>> clusters = new ArrayList<List<List<Integer>>>(k);
-//		for(int i=0;i<k;i++){
-//			clusters.add(new ArrayList<List<Integer>>());
-//		}
-//		for(int i=0;i<points.size();i++){
-//			List<Integer> point = points.get(i);
-//			int assign = assigns.get(i);
-//			clusters.get(assign).add(point);
-//		}
-//		return clusters;
-//	}
-//	
-//	public List<Integer> approximation(List<List<Integer>> _points, int k){
-//		List<Pair<List<Integer>,Integer>> entries = new ArrayList<Pair<List<Integer>, Integer>>();
-//		for(List<Integer> point : _points){
-//			int t = 0;
-//			for(int v : point){
-//				if(v > 0){
-//					t++;
-//				}
-//			}
-//			entries.add(new Pair<List<Integer>,Integer>(point, t));
-//		}
-//		Collections.sort(entries, new Comparator<Pair<List<Integer>, Integer>>() {
-//			@Override
-//			public int compare(
-//					Pair<List<Integer>,Integer> entry1, Pair<List<Integer>,Integer> entry2) {
-//	                	return (entry2.y).compareTo(entry1.y);
-//	            	}
-//		});
-//		int N = 100;
-//		List<List<Integer>> points = new ArrayList<List<Integer>>(N);
-//		for(int i=0;i<N;i++){
-//			Pair<List<Integer>, Integer> p = entries.get(i);
-//			System.out.println(p.y);
-//			points.add(p.x);
-//		}
-//		
-//		List<Integer> assigns = new ArrayList<Integer>(points.size());
-//		for(int i=0;i<points.size();i++){
-//			assigns.add(0);
-//		}
-//		double minVal = calcF(assignsToClusters(points, assigns, k));
-//		for(int count=0;;count++){
-//			List<Integer> best = null;
-//			for(int i=0;i<assigns.size();i++){
-//				int assign = assigns.get(i);
-//				if(assign != 0){
-//					continue;
-//				}
-//				for(int j=1;j<k;j++){
-//					List<Integer> cp = new ArrayList<Integer>(assigns.size());
-//					for(int v : assigns){
-//						cp.add(v);
-//					}
-//					cp.set(i, j);
-//					double f = calcF(assignsToClusters(points, cp, k));
-//					if(f < minVal){
-//						best = cp;
-//						minVal = f;
-//						System.out.println(minVal);
-//					}
-//				}
-//			}
-//			if(best == null){
-//				System.out.println("num of iterations: " + count);
-//				return assigns;
-//			}
-//			assigns = best;
-//		}
-//	}
-//	
-//	public List<Integer> start(List<List<Integer>> _points, int k){
-//		List<Pair<List<Integer>,Integer>> entries = new ArrayList<Pair<List<Integer>, Integer>>();
-//		for(List<Integer> point : _points){
-//			int t = 0;
-//			for(int v : point){
-//				if(v > 0){
-//					t++;
-//				}
-//			}
-//			entries.add(new Pair<List<Integer>,Integer>(point, t));
-//		}
-//		Collections.sort(entries, new Comparator<Pair<List<Integer>, Integer>>() {
-//			@Override
-//			public int compare(
-//					Pair<List<Integer>,Integer> entry1, Pair<List<Integer>,Integer> entry2) {
-//	                	return (entry2.y).compareTo(entry1.y);
-//	            	}
-//		});
-//		int N = 100;
-//		List<List<Integer>> points = new ArrayList<List<Integer>>(_points.size());
-//		List<Integer> assigns = new ArrayList<Integer>(_points.size());
-//		for(int n=0; n < entries.size(); n+=N){
-//			for(int i=n;i<N + n && i<entries.size();i++){
-//				Pair<List<Integer>, Integer> p = entries.get(i);
-//				points.add(p.x);
-//			}
-//			assigns = inner(points, k, assigns);
-//		}
-//		return assigns;
-//	}
-//	public List<Integer> inner(List<List<Integer>> points, int k, List<Integer> assigns){
-//		int currentSize = assigns.size();
-//		int newPointsSize = points.size() - currentSize;
-//		for(int i=0;i<newPointsSize;i++){
-//			assigns.add(0);
-//		}
-//		double minVal = calcF2(points, assigns, k);
-//		for(int count=0;;count++){
-//			List<Integer> best = null;
-//			for(int i=currentSize;i<assigns.size();i++){
-//				int assign = assigns.get(i);
-//				if(assign != 0){
-//					continue;
-//				}
-//				for(int j=1;j<k;j++){
-//					List<Integer> cp = new ArrayList<Integer>(assigns.size());
-//					for(int v : assigns){
-//						cp.add(v);
-//					}
-//					cp.set(i, j);
-//					double f = calcF2(points, cp, k);
-//					if(f < minVal){
-//						best = cp;
-//						minVal = f;
-//						System.out.println(minVal);
-//					}
-//				}
-//			}
-//			if(best == null){
-//				System.out.println("num of iterations: " + count);
-//				return assigns;
-//			}
-//			assigns = best;
-//		}
-//	}
-//	
 	
-	public List<Integer> start2(List<List<Integer>> _points, int k){
+	/**
+	 * ベクトル中の非ゼロの要素数の数が大きい順に並び変える
+	 * 
+	 * @param _points
+	 * @return
+	 */
+	private List<List<Integer>> sortPoints(List<List<Integer>> _points){
 		List<Pair<List<Integer>,Integer>> entries = new ArrayList<Pair<List<Integer>, Integer>>();
 		for(List<Integer> point : _points){
 			int t = 0;
@@ -198,35 +40,65 @@ public class Partition {
 		for(Pair<List<Integer>, Integer> p : entries){
 			points.add(p.x);
 		}
-		return inner3(points, k);
+		return points;
+	}
+	public List<Integer> start(List<List<Integer>> _points, int k){
+		List<List<Integer>> points = sortPoints(_points);
+		return assign(points, k);
 	}
 	
 	
+	/**
+	 * ベクトルの引き算 x = x - y
+	 * @param x
+	 * @param y
+	 */
 	private void sub(List<Integer> x, List<Integer> y){
 		for(int i=0;i<x.size();i++){
 			x.set(i, x.get(i) - y.get(i));
 		}
 	}
+	/**
+	 * ベクトルの足し算 x = x + y
+	 * @param x
+	 * @param y
+	 */
 	private void add(List<Integer> x, List<Integer> y){
 		for(int i=0;i<x.size();i++){
 			x.set(i, x.get(i) + y.get(i));
 		}
 	}
 	
-	private int calcF3(List<List<Integer>> folded){
+	/**
+	 * 各clusterの評価(Eval(cluster))はそのcluster出現する非ゼロの要素数とする（少ないほどよい）
+	 * 分割全体の評価(Eval(folded))は全てのclusterについてEval(cluster)の最大値とする（少ないほどよい）
+	 * @param folded
+	 * @return
+	 */
+	private int Eval(List<List<Integer>> folded){
 		int ret = 0;
-		for(List<Integer> t : folded){
-			int s = 0;
-			for(int v : t){
-				s += v > 0 ? 1 : 0;
+		for(List<Integer> cluster : folded){
+			int e = 0;
+			for(int v : cluster){
+				e += v > 0 ? 1 : 0;
 			}
-			if(s > ret){
-				ret = s;
+			if(e > ret){
+				ret = e;
 			}
 		}
 		return ret;
 	}
-	public List<Integer> inner3(List<List<Integer>> points, int k){
+
+	
+	/**
+	 * roots(points)ををどう分割するか計算する。
+	 * i番目のrootは返値のListのi番目（ret.get(i)）に割り振る
+	 * 
+	 * @param points
+	 * @param k
+	 * @return
+	 */
+	public List<Integer> assign(List<List<Integer>> points, int k){
 		List<Integer> assigns = new ArrayList<Integer>(points.size());
 		for(int i=0;i<points.size();i++){
 			assigns.add(0);
@@ -234,8 +106,14 @@ public class Partition {
 
 		int l = points.get(0).size();
 		
+		/**
+		 * clusterの集合をfoldedとする。
+		 * 各clusterは、そのclusterに含まれる全てのpoint(ベクトル)のj番目の要素の和をとったベクトルである。
+		 * 初期状態では全てのベクトルがcluster0に含まれている。
+		 * 
+		 * 要素の和をとっているのは高速化のためであり、本来は各clusterはベクトルの集合として定義する。
+		 */
 		List<List<Integer>> folded = new ArrayList<List<Integer>>(k);
-		
 		// cluster 0
 		List<Integer> t1 = new ArrayList<Integer>(l);
 		for(int j=0;j<l;j++){
@@ -256,17 +134,22 @@ public class Partition {
 			folded.add(t);
 		}
 
-		int minVal = calcF3(folded);
+		// Eval(folded)が小さくなるように局所最適解を求める
+		// 非ゼロの要素数が大きいものからcluster0から別のcluster j へ移動していく
+		int minVal = Eval(folded);
+		System.out.println("minVal = " + minVal);
 		for(int i=0;i<points.size();i++){
 			List<Integer> point = points.get(i);
-			sub(folded.get(0), points.get(i));
+			sub(folded.get(0), point);
 			int move = 0;
 			for (int j = 1; j < k; j++) {
 				add(folded.get(j), point);
-				int f = calcF3(folded);
+				int f = Eval(folded);
+				System.out.println("f = " + f);
 				if (f < minVal) {
 					minVal = f;
 					move = j;
+					System.out.println("________________________" + "move " + move);
 				}
 				sub(folded.get(j), point);
 			}
@@ -275,74 +158,4 @@ public class Partition {
 		}
 		return assigns;
 	}
-	
-//	public List<Integer> inner2(List<List<Integer>> points, int k){
-//		int l = points.get(0).size();
-//		List<Integer> assigns = new ArrayList<Integer>(points.size());
-//		for(int i=0;i<points.size();i++){
-//			assigns.add(0);
-//		}
-//		
-//		List<List<Integer>> folded = new ArrayList<List<Integer>>(k - 1);
-//		for(int i=0;i<k-1;i++){
-//			List<Integer> t = new ArrayList<Integer>(l);
-//			for(int j=0;j<l;j++){
-//				t.add(0);
-//			}
-//			folded.add(t);
-//		}
-//
-//		double minVal = calcF2(points, assigns, k);
-//		for(int i=0;i<points.size();i++){
-//			System.out.println("point " + i);
-//			for (int j = 1; j < k; j++) {
-//				List<Integer> cp = new ArrayList<Integer>(assigns);
-//				cp.set(i, j);
-//				double f = calcF2(points, cp, k);
-//				if (f < minVal) {
-//					assigns = cp;
-//					minVal = f;
-//					System.out.println(minVal);
-//				}
-//			}
-//		}
-//		return assigns;
-//	}
-//	
-//	private List<List<Integer>> convertAssigns(List<Integer> assigns, int k){
-//		List<List<Integer>> ret = new ArrayList<List<Integer>>(k);
-//		for(int i=0;i<k;i++){
-//			ret.add(new ArrayList<Integer>());
-//		}
-//		for(int i=0;i<assigns.size();i++){
-//			int assign = assigns.get(i);
-//			ret.get(assign).add(i);
-//		}
-//		return ret;
-//	}
-//
-//	private double calcF2(List<List<Integer>> points, List<Integer> assigns, int k){
-//		List<List<Integer>> assigns2 = convertAssigns(assigns, k);
-//		double ret = 0.0;
-//		int l = points.get(0).size();
-//		for(List<Integer> aa : assigns2){
-//			if(aa.isEmpty()){
-//				continue;
-//			}
-//			int r = 0;
-//			for(int i=0;i<l;i++){
-//				for(int pIndex : aa){
-//					List<Integer> point = points.get(pIndex);
-//					if(point.get(i) > 0){
-//						r++;
-//						break;
-//					}
-//				}
-//			}
-//			ret += Math.pow(r, 2);
-//		}
-//		return ret;
-//	}
-	
-
 }
--- src/main/java/com/github/cloverrose/jarsplit/split/Spliter.java
+++ src/main/java/com/github/cloverrose/jarsplit/split/Spliter.java
@@ -2,7 +2,6 @@ package com.github.cloverrose.jarsplit.split;
 
 import java.util.*;
 
-import com.github.cloverrose.jarsplit.kmeans.Kmeans;
 import com.github.cloverrose.jarsplit.scc.*;
 import com.github.cloverrose.jarsplit.partition.*;
 
@@ -19,23 +18,16 @@ public class Spliter {
 			}
 		}
 	}
-	
-//	private void addDummyNode(Graph<String> graph, int numRoots){
-//		Set<String> roots = graph.getRoots();
-//		int n = roots.size() / numRoots;
-//		int c = 0;
-//		int i = 0;
-//		for(String root : roots){
-//			graph.addEdge("DUMMY" + c, root);
-//			i++;
-//			if(i == n){
-//				c++;
-//				i=0;
-//			}
-//		}
-//	}
-	
 
+	/**
+	 * 依存関係graphを簡単に解析して、DUMMYノードを追加して
+	 * root要素をそのDUMMYノードの子要素にする。
+	 * 
+	 * 簡単に解析とは：root要素とその直接の子要素のみを見て、
+	 * 子要素が似ているroot要素同士を同じクラスタに分類する
+	 * @param graph
+	 * @param numRoots
+	 */
 	private void partition(Graph<String> graph, int numRoots){
 		Set<Integer> _roots = graph.getRootIndices();
 		List<Integer> roots = new ArrayList<Integer>(_roots);
@@ -47,7 +39,14 @@ public class Spliter {
 				_children.put(child, _children.containsKey(child) ? _children.get(child) + 1 : 1);
 			}
 		}
-	    List<Integer> children = new ArrayList<Integer>(_children.keySet());
+		// 全てのrootに参照されているものを取り除く
+	    List<Integer> children = new ArrayList<Integer>(_children.size());
+	    for(Map.Entry<Integer, Integer> e : _children.entrySet()){
+			if(e.getValue() != num){
+				children.add(e.getKey());
+			}
+		}
+
 	    int dimension = children.size();
 		System.out.println("Dimension = " + dimension);
 		System.out.println("Num = " + num);		
@@ -66,9 +65,11 @@ public class Spliter {
 			points.add(point);
 		}
 		
-		List<Integer> assigns = new Partition().start2(points, numRoots);
+		List<Integer> assigns = new Partition().start(points, numRoots);
 		System.out.println(assigns);
 
+		// DUMMYノードを追加し、
+		// Partitionで求めた分割に従って、DUMMYノードに割り振る
 		for(int i=0; i<numRoots; i++){
 			for(int j=0;j<assigns.size();j++){
 				int assign = assigns.get(j);
@@ -77,79 +78,7 @@ public class Spliter {
 				}
 			}
 		}
-	}
-	
-//	private void simplifyGraphByFirstChildren(Graph<String> graph, int numRoots){
-//		Set<Integer> _roots = graph.getRootIndices();
-//		Map<Integer, Integer> _children = new THashMap<Integer, Integer>();
-//		for(Integer vertex : _roots){
-//			for(Integer child : graph.getAdjacentVertexIndices(vertex)){
-//				if(_children.containsKey(child)){
-//					_children.put(child, _children.get(child) + 1);
-//				}else{
-//					_children.put(child, 1);
-//				}
-//			}
-//		}
-//		List<Map.Entry<Integer,Integer>> entries = new ArrayList<Map.Entry<Integer, Integer>>(_children.entrySet());
-//		Collections.sort(entries, new Comparator<Map.Entry<Integer, Integer>>() {
-//			@Override
-//			public int compare(
-//					Map.Entry<Integer,Integer> entry1, Map.Entry<Integer,Integer> entry2) {
-//	                	return ((Integer)entry2.getValue()).compareTo((Integer)entry1.getValue());
-//	            	}
-//		});
-//	    List<Integer> children = new ArrayList<Integer>(100);
-//	    int cc = 0;
-//	    for(Map.Entry<Integer, Integer> e : entries){
-//	    	if(e.getValue() == 1){
-//	    		break;
-//	    	}
-//	    	children.add(e.getKey());
-//	    	cc++;
-//	    	if(cc == 100){
-//	    		break;
-//	    	}
-//	    }
-//		List<Integer> roots = new ArrayList<Integer>(_roots);
-//		// List<Integer> children = new ArrayList<Integer>(_children);
-//		int dimension = children.size();
-//		int num = roots.size();
-//		System.out.println("Dimension = " + dimension);
-//		System.out.println("Num = " + num);		
-//		List<List<Double>> points = new ArrayList<List<Double>>(num);
-//		for(int i=0;i<num;i++){
-//			if(i % 1000 == 0){
-//				System.out.println(i);
-//			}
-//			List<Double> point = new ArrayList<Double>(dimension);
-//			for(int j=0;j<dimension;j++){
-//				point.add(0.0);
-//			}
-//			for(Integer child : graph.getAdjacentVertexIndices(roots.get(i))){
-//				int j = children.indexOf(child);
-//				if(j != -1){
-//					point.set(j, 1.0);
-//				}
-//			}
-//			points.add(point);
-//		}
-//		
-//		List<Integer> assigns = new Kmeans().start(points, numRoots);
-//
-//		
-//		for(int i=0; i<numRoots; i++){
-//			for(int j=0;j<assigns.size();j++){
-//				int assign = assigns.get(j);
-//				if(assign == i){
-//					graph.addEdge("DUMMY" + i, graph.fromIndex(roots.get(j)));
-//				}
-//			}
-//		}
-//	}
-
-
-	
+	}	
 	
 	public List<Set<String>> split(Map<String, Set<String>> dependency, Map<String, Set<String>> super2subs, int numRoots){
 		// addEdge owner -> instance
@@ -183,78 +112,15 @@ public class Spliter {
 		}
 
 		System.err.println("First Graph");
-		// System.err.println(graph.toStringIndex());
 		Set<String> roots = graph.getRoots();
 		System.out.println(roots.size());
 		System.out.println("VERTEX NUM " + graph.getVertexes().size());
 		
-		// simplifyGraphByFirstChildren(graph, numRoots);
-		// addDummyNode(graph, numRoots);
 		partition(graph, numRoots);
+		
+		// DFSをしてDUMMYノードから到達可能な要素を全て計算する
 		List<Set<String>> ret = new DFS().dfs(graph, graph.getRoots());
 		
-//		Graph<Set<String>> simpleGraph = new Simplifier<String>().simplify(graph, roots);
-//		
-//		System.err.println("\nFinal Graph");
-//		System.out.println(simpleGraph.toStringIndex());
-//		Set<Set<String>> simpleRoots = simpleGraph.getRoots();
-//		System.err.println(simpleRoots.size());
-		
-		//---
-//		Set<Integer> rootIndices = simpleGraph.getRootIndices();
-//		List<Set<Integer>> attrs = new DepthFirstSearch<Set<String>>().depthfirstsearchs(simpleGraph, rootIndices);
-////		
-//		Set<Integer> rootIndices = graph.getRootIndices();
-//		List<Set<Integer>> attrs = new DepthFirstSearch<String>().depthfirstsearchs(graph, rootIndices);
-//		
-//		for(int v : rootIndices){
-//			System.out.println(v + " : " + attrs.get(v));
-//		}
-		
-//		if(rootIndices.size() <= numRoots){
-//			List<Set<String>> ret = new ArrayList<Set<String>>(rootIndices.size());
-//			for(int v : rootIndices){
-//				Set<String> temp = new THashSet<String>(attrs.get(v).size());
-//				for(int u : attrs.get(v)){
-//					temp.addAll(simpleGraph.fromIndex(u));
-//				}
-//				ret.add(temp);
-//			}
-//			return ret;
-//		}
-		
-//		int size = simpleGraph.getVertexes().size();
-//		List<List<Double>> points = new ArrayList<List<Double>>(rootIndices.size());
-//		Map<Integer, Integer> association = new THashMap<Integer, Integer>();
-//		int c = 0;
-//		for(int v : rootIndices){
-//			List<Double> point = new ArrayList<Double>(size);
-//			for(int i=0;i<size;i++){
-//				if(attrs.get(v).contains(i)){
-//					point.add(1.0);
-//				}else{
-//					point.add(0.0);
-//				}
-//			}
-//			points.add(point);
-//			association.put(c, v);
-//			c++;
-//			// System.out.println(point);
-//		}
-//		List<Integer> assigns = new Kmeans().start(points, numRoots);
-//		System.out.println(assigns);
-//		List<Set<String>> ret = new ArrayList<Set<String>>(numRoots);
-//		for(int i=0;i<numRoots;i++){
-//			ret.add(new THashSet<String>());
-//		}
-//		for(int i=0;i<assigns.size();i++){
-//			int assign = assigns.get(i);
-//			int v = association.get(i);
-//			for(Integer u : attrs.get(v)){
-//				ret.get(assign).addAll(simpleGraph.fromIndex(u));
-//			}
-//		}
 		return ret;
-		//---
 	}
 }
